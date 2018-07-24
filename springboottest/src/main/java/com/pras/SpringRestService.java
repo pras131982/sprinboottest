487b423005e3ae9e02f5a11924d3d0de484a4803
Prashant <Prashant@prashants-mbp.fios-router.home>
2018-07-23 19:27:06 -0500

    Initial commit springboottest

---
+++ springboottest/src/main/java/com/pras/SpringRestService.java
@@ -0,0 +1,14 @@
+package com.pras;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class SpringRestService {
+
+	@RequestMapping("/prashello")
+	public String index() {
+		return "Greetings from Pras Spring Boot!";
+	}
+
+}
